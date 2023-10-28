8122379f0e52d6b0fd01f3cd180c4f4c1c4898bc
Karma64 <87253582+Karma64@users.noreply.github.com>
2023-10-28 17:57:52 +0530

    Added DeleteAccount Method in BankingMain

--- BankingMain.java
+++ BankingMain.java
@@ -150,3 +150,136 @@ public class JavaProject extends User{
 		}while(options!=0);
 	}
 
+private static void deleteAccount(String uid) throws IOException {
+		/**
+		 * Method will help user to delete its account
+		 * 
+		 */
+		String storeSavingaccno="",storeCurrentaccno="",storeSavingBal="",storeCurrentBal="";
+			File file = new File(uid);
+			Scanner in = new Scanner(file);
+			String filecontents="";
+			while(in.hasNext())
+			{
+				filecontents = filecontents + in.nextLine() + "\n";
+			}
+			String[] strlist = filecontents.split("\n");
+			int flagSavingBal=0,flagCurrentBal=0;
+			for (String str : strlist)
+			{
+				if(str.startsWith("Savings Account no :"))
+				{
+					storeSavingaccno = finddigit(str);
+					if(Integer.parseInt(storeSavingaccno)>0)
+					{
+						flagSavingBal=1;
+					}
+				}
+				if(str.startsWith("Current Account no :"))
+				{
+					 storeCurrentaccno = finddigit(str);
+					 if(Integer.parseInt(storeCurrentaccno)>0)
+					 {
+							flagCurrentBal=1;
+					 }
+				}
+				if(str.startsWith("Saving Account Balance :"))
+				{
+					if(flagSavingBal==1)
+					{
+						storeSavingBal = finddigit(str);
+					}
+				}
+				if(str.startsWith("Current Account Balance :"))
+				{
+					if(flagCurrentBal==1)
+					{
+						storeCurrentBal = finddigit(str);
+					}
+				}
+			}
+			in.close();
+			if(flagSavingBal==1 && flagCurrentBal==1)
+			{
+				String choice = JOptionPane.showInputDialog("Enter 1. To delete Savings account\nEnter 2. To delete Current account\n "
+						+"Enter 3. To delete all of your accounts ");
+				if(Integer.parseInt(choice)==1)
+				{
+					String confirm =  JOptionPane.showInputDialog("Are you sure you want to delete your Saving account."
+							+"\nEnter Y. To confirm \n Enter X. To Discard");
+					if(confirm.equalsIgnoreCase("y"))
+					{
+						User u = new User();
+						u.Update(uid, storeSavingaccno, "0", "Savings Account no :", "delete");
+						u.Update(uid, storeSavingBal, "0", "Saving Account Balance :", "delete");
+						JOptionPane.showMessageDialog(null, "You Successfully deleted your Savings Account");
+					}
+					else
+					{
+						JOptionPane.showMessageDialog(null,"It is a nice decision to have a savings account");
+					}
+				}
+				else if(Integer.parseInt(choice)==2)
+				{
+					String confirm =  JOptionPane.showInputDialog("Are you sure you want to delete your Current account."
+							+"\nEnter Y. To confirm \n Enter X. To Discard");
+					if(confirm.equalsIgnoreCase("y"))
+					{
+						User u = new User();
+						u.Update(uid, storeCurrentaccno, "0", "Current Account no :", "delete");
+						u.Update(uid, storeCurrentBal, "0", "Current Account Balance :", "delete");
+						JOptionPane.showMessageDialog(null, "You Successfully deleted your Current Account");
+					}
+					else
+					{
+						JOptionPane.showMessageDialog(null,"It is a nice decision to have a current account");
+					}
+				}
+				else if(Integer.parseInt(choice)==3)
+				{
+					String confirm =  JOptionPane.showInputDialog("Are you sure you want to delete your accounts."
+							+"\nEnter Y. To confirm \n Enter X. To Discard");
+					if(confirm.equalsIgnoreCase("y"))
+					{
+						file.delete();
+						//File delfile = new File(uid);
+						//file.deleteOnExit();
+							JOptionPane.showMessageDialog(null, "Thank you for banking with us. BYE");
+						//}
+					}
+					else
+					{
+						JOptionPane.showMessageDialog(null,"It is a nice decision to continue banking with us");
+					}
+				}
+			}
+			else if(flagSavingBal==1)
+			{
+				String confirm =  JOptionPane.showInputDialog("Are you sure you want to delete your Saving account."
+						+"\nEnter Y. To confirm \n Enter X. To Discard");
+				if(confirm.equalsIgnoreCase("y"))
+				{
+						file.delete();
+						JOptionPane.showMessageDialog(null, "Thank you for banking with us. BYE");
+				}
+				else
+				{
+					JOptionPane.showMessageDialog(null,"\"It is a nice decision to continue banking with us");
+				}
+			}
+			else if(flagCurrentBal==1)
+			{
+				String confirm =  JOptionPane.showInputDialog("Are you sure you want to delete your Current account."
+						+"\nEnter Y. To confirm \n Enter X. To Discard");
+				if(confirm.equalsIgnoreCase("y"))
+				{
+					file.delete();
+					JOptionPane.showMessageDialog(null, "Thank you for banking with us. BYE");
+				}
+				else
+				{
+					JOptionPane.showMessageDialog(null,"\"It is a nice decision to continue banking with us");
+				}
+			}
+
+	}
